241bf7fcfbd9e33636acd4b65f2cff01b60464ad
Luigi Remor <luigiremor@github.com>
2022-03-17 23:37:56 -0300

    Fixing chart UI

--- components/Graph copy.js
+++ components/Graph copy.js
@@ -15,45 +15,48 @@ import data from '../../weather_backend/data.json'
 
 export default function GraphT() {
   return (
-    <div className="h-screen w-full">
-      {data ? (
-        <ResponsiveContainer width="90%" height={400}>
-          <AreaChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5
-            }}
-          >
-            <defs>
-              <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#0077f6" stopOpacity={0.4}></stop>
-                <stop
-                  offset="75%"
-                  stopColor="#0077f6"
-                  stopOpacity={0.05}
-                ></stop>
-              </linearGradient>
-            </defs>
-            <CartesianGrid opacity={0.1} vertical={false} />
-            <XAxis dataKey="timestamp" />
-            <YAxis />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="t2"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
+    <ResponsiveContainer width="90%" height={400}>
+      <AreaChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <defs>
+          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#0077f6" stopOpacity={0.4}></stop>
+            <stop offset="75%" stopColor="#0077f6" stopOpacity={0.05}></stop>
+          </linearGradient>
+        </defs>
+        <CartesianGrid opacity={0.1} vertical={false} />
+        <XAxis dataKey="timestamp" axisLine={false} tickLine={false} />
+        <YAxis axisLine={false} tickLine={false} tickCount={4} />
+        <Tooltip content={<CustomTooltip />} />
+        <Area
+          type="monotone"
+          dataKey="t2"
+          stroke="#0077f6"
+          activeDot={{ r: 5 }}
+          fill="url(#color)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
   )
+  function CustomTooltip({ active, payload, label }) {
+    console.log(payload)
+    if (active) {
+      return (
+        <div className="rounded-md bg-white shadow-2xl text-center p-2">
+          <div>{label}</div>
+          <p>Temperatura: {payload[0].value.toFixed(1)} ºC</p>
+        </div>
+      )
+    }
+    return null
+  }
 }
